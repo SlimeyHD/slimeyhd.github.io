013ac02d38a3efd2fdc0a4c6f45e26a44abd40ed
SlimeyHD <127538351+SlimeyHD@users.noreply.github.com>
2023-03-11 12:43:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-
-SlimeyHD
-/
-slimeyhd.github.io
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-slimeyhd.github.io/script.js
-@SlimeyHD
-SlimeyHD Update script.js
- 1 contributor
-206 lines (192 sloc)  6.3 KB
 gold = Number(localStorage.getItem('gold')) || 0
-up1cost = Number(localStorage.getItem('up1cost')) || 10 
+up1cost = Number(localStorage.getItem('up1cost')) || 10
 up1total = Number(localStorage.getItem('up1total')) || 0
-up2cost = Number(localStorage.getItem('up2cost')) || 10 
+up2cost = Number(localStorage.getItem('up2cost')) || 10
 up2total = Number(localStorage.getItem('up2total')) || 0
 up1scaling = Number(localStorage.getItem('up1scaling')) || 1.5
 up1benefit = Number(localStorage.getItem('up1benefit')) || 1
@@ -37,28 +18,33 @@ mup4cost = Number(localStorage.getItem('mup4cost')) || 50
 mup4total = Number(localStorage.getItem('mup4total')) || 0
 mgoldmulti = Number(localStorage.getItem('mgoldmulti')) || 1
 magic = Number(localStorage.getItem('magic')) || 0
-
+  var resetdiv = document.getElementById("resetdiv");
+  var MagicLayer1 = document.getElementById("MagicLayer1");
+  var MainFrame = document.getElementById("MainFrame");
+  var IronFrame = document.getElementById("IronFrame");
+  var IronButton = document.getElementById("IronMenuButton");
+  var UnlockIronMenuButton = document.getElementById("UnlockIronMenuButton")
 
 const abbrNum = (number, decPlaces) => {
   if (number >= 1e34) {
-      return number
-      
-  }else if(number <= 1000){
-  return number
+    return number
+
+  } else if (number <= 1000) {
+    return number
   }
   decPlaces = Math.pow(10, decPlaces)
   var abbrev = ['K', 'M', 'B', 'T', 'Qa', 'Qt', 'Sx', 'Sp', 'Oc', 'No']
   for (var i = abbrev.length - 1; i >= 0; i--) {
-      var size = Math.pow(10, (i + 1) * 3)
-      if (size <= number) {
-          number = Math.round((number * decPlaces) / size) / decPlaces
-          if (number == 1000 && i < abbrev.length - 1) {
-              number = 1
-              i++
-          }
-          number += abbrev[i]
-          break
+    var size = Math.pow(10, (i + 1) * 3)
+    if (size <= number) {
+      number = Math.round((number * decPlaces) / size) / decPlaces
+      if (number == 1000 && i < abbrev.length - 1) {
+        number = 1
+        i++
       }
+      number += abbrev[i]
+      break
+    }
   }
   return number
 }
@@ -68,18 +54,19 @@ update()
 var intervalID = window.setInterval(myCallback, 100);
 
 function myCallback() {
-  gold = (gold + ((up1total * up1benefit) +1) * mgoldmulti)
+  gold = (gold + ((up1total * up1benefit) + 1) * mgoldmulti)
   update()
 }
+
 function buyup1() {
   if (up1cost - 1 < gold) {
     gold = gold - up1cost
     up1total = up1total + 1
-    up1cost = Math.round(10 * Math.pow(up1scaling,up1total - 1))
+    up1cost = Math.round(10 * Math.pow(up1scaling, up1total - 1))
     update()
   }
 
- 
+
 }
 function buyup2() {
   if (up2cost - 1 < up1total) {
@@ -88,20 +75,20 @@ function buyup2() {
     up2cost = Math.round(up2cost * 1.2)
     up2total = up2total + 1
     up1scaling = 1.5 + (up2penalty * up2total)
-    up1benefit = (1 + mup2total) * Math.pow(up2benefit , up2total)
+    up1benefit = (1 + mup2total) * Math.pow(up2benefit, up2total)
     gold = 0
-update()
+    update()
   }
 }
 function reset1() {
-  if (gold > 9999 ) {
+  if (gold > 9999) {
     up1cost = 10
     up1total = 0
     up2cost = 10
     up2total = 0
     up1scaling = 1.6
     up1benefit = 1 + mup2total
-    magic = magic + Math.round(Math.pow(gold , 0.5) /50)
+    magic = magic + Math.round(Math.pow(gold, 0.5) / 50)
     gold = 0
     update()
   }
@@ -111,18 +98,18 @@ function buymup1() {
     magic = magic - mup1cost
     mup1cost = Math.round(mup1cost * 1.5)
     mup1total = mup1total + 1
-    mgoldmulti = mgoldmulti +1
+    mgoldmulti = mgoldmulti + 1
     update()
   }
 }
 function buymup2() {
-   if (magic > mup2cost - 1) {
+  if (magic > mup2cost - 1) {
     magic = magic - mup2cost
     mup2cost = Math.round(mup2cost * 1.6)
     mup2total = mup2total + 1
-    up1benefit = (1 + mup2total) * Math.pow(up2benefit , up2total)
-     update()
-   }
+    up1benefit = (1 + mup2total) * Math.pow(up2benefit, up2total)
+    update()
+  }
 }
 function buymup3() {
   if (magic > mup3cost - 1) {
@@ -130,8 +117,8 @@ function buymup3() {
     mup3cost = Math.round(mup3cost * 1.4)
     mup3total = mup3total + 1
     up2benefit = up2benefit + 0.1
-    up1benefit = (1 + mup2total) * Math.pow(up2benefit , up2total)
-update()
+    up1benefit = (1 + mup2total) * Math.pow(up2benefit, up2total)
+    update()
   }
 }
 function buymup4() {
@@ -142,46 +129,59 @@ function buymup4() {
       mup4total = mup4total + 1
       up2penalty = up2penalty - 0.01
       up1scaling = 1.5 + (up2penalty * up2total)
-      up1cost = Math.round(10 * Math.pow(up1scaling,up1total - 1))
+      up1cost = Math.round(10 * Math.pow(up1scaling, up1total - 1))
       update()
     }
   }
 }
 
+function openMainMenu() {
+  IronFrame.style.display = "none"
+  MainFrame.style.display = "Block"
+}
 
-  
+function openIronMenu() {
+  IronFrame.style.display = "Block"
+  MainFrame.style.display = "none"
+}
+
+
+
+
 function die() {
-gold = 0
-up1cost = 10 
-up1total = 0
-up2cost = 10 
-up2total = 0
-up1scaling = 1.5
-up1benefit = 1
-up2benefit = 1.5
-up2penalty = 0.1
-magic = 0
-mup1cost = 1
-mup1total = 0
-mup2cost = 5
-mup2total = 0
-mup3cost = 15
-mup3total = 0
-mup4cost = 50
-mup4total = 0
-mgoldmulti = 1
-update()
+  gold = 0
+  up1cost = 10
+  up1total = 0
+  up2cost = 10
+  up2total = 0
+  up1scaling = 1.5
+  up1benefit = 1
+  up2benefit = 1.5
+  up2penalty = 0.1
+  magic = 0
+  mup1cost = 1
+  mup1total = 0
+  mup2cost = 5
+  mup2total = 0
+  mup3cost = 15
+  mup3total = 0
+  mup4cost = 50
+  mup4total = 0
+  mgoldmulti = 1
+
+
+  update()
 }
 
 function update() {
-  document.getElementById("earngoldgold").innerHTML = abbrNum(Math.round(((up1total * up1benefit) +1) * mgoldmulti * 10), 2)
+  document.getElementById("earngoldgold").innerHTML = abbrNum(Math.round(((up1total * up1benefit) + 1) * mgoldmulti * 10), 2)
   document.getElementById("goldamount").innerHTML = abbrNum(Math.round(gold), 2)
-  document.getElementById("up1total").innerHTML = up1total
-  document.getElementById("up1cost").innerHTML = up1cost
-  document.getElementById("up2cost").innerHTML = up2cost
-  document.getElementById("up2total").innerHTML = up2total
-  document.getElementById("up1effect").innerHTML = Math.round(up1benefit)
-  document.getElementById("magicamount").innerHTML = magic
+  document.getElementById("up1total").innerHTML = abbrNum(up1total, 2)
+  document.getElementById("up1cost").innerHTML = abbrNum(up1cost, 2)
+  document.getElementById("up2cost").innerHTML = abbrNum(up2cost, 2)
+  document.getElementById("up2total").innerHTML = abbrNum(up2total, 2)
+  document.getElementById("up1effect").innerHTML = abbrNum(Math.round(up1benefit), 2)
+  document.getElementById("magicamount").innerHTML = abbrNum(magic, 2)
   document.getElementById("mup1cost").innerHTML = mup1cost
   document.getElementById("mup1total").innerHTML = mup1total
   document.getElementById("mup2cost").innerHTML = mup2cost
@@ -191,8 +191,8 @@ function update() {
   document.getElementById("mup4cost").innerHTML = mup4cost
   document.getElementById("mup4total").innerHTML = mup4total
   document.getElementById("up2benefit").innerHTML = up2benefit.toFixed(1)
-  document.getElementById("magicgain").innerHTML = 
-  Math.floor(Math.pow(gold,0.5)/50)  
+  document.getElementById("magicgain").innerHTML =
+    Math.floor(Math.pow(gold, 0.5) / 50)
   localStorage.setItem('gold', gold)
   localStorage.setItem('up1total', up1total)
   localStorage.setItem('up1cost', up1cost)
@@ -212,13 +212,12 @@ function update() {
   localStorage.setItem('mup4total', mup4total)
   localStorage.setItem('mup4cost', mup4cost)
   localStorage.setItem('mgoldmulti', mgoldmulti)
-setTimeout(1000)
-  var magicButtons = document.getElementById("MagicLayerUnlock1");
-  var resetdiv = document.getElementById("resetdiv");
-    if (gold>=10000 || mup1total >= 1 || magic >= 1) {
-            magicButtons.style.zIndex = "-1";
-      magicButtons.innerText = ""
-    } else {
 
-    }
+  if (gold >= 10000 || mup1total >= 1 || magic >= 1) {
+    IronButton.style.display = "block"
+    UnlockIronMenuButton.style.display = "none"
+  }else{
+    IronButton.style.display = "none"
+    UnlockIronMenuButton.style.display = "block"
+  }
 }
